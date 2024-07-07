565500e0dffcc6a81f8d2ca02a7a23045ba9cd09
Razanmtw17 <93105958+Razanmtw17@users.noreply.github.com>
2024-07-07 23:53:25 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.zerobutton = new System.Windows.Forms.Button();
+            this.dotbutton = new System.Windows.Forms.Button();
+            this.deletebutton = new System.Windows.Forms.Button();
+            this.equalbutton = new System.Windows.Forms.Button();
+            this.threebutton = new System.Windows.Forms.Button();
+            this.twobutton = new System.Windows.Forms.Button();
+            this.onebutton = new System.Windows.Forms.Button();
+            this.sixbutton = new System.Windows.Forms.Button();
+            this.fivebutton = new System.Windows.Forms.Button();
+            this.fourbutton = new System.Windows.Forms.Button();
+            this.mulbutton = new System.Windows.Forms.Button();
+            this.minusbutton = new System.Windows.Forms.Button();
+            this.plusbutton = new System.Windows.Forms.Button();
+            this.dividebutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ninebutton = new System.Windows.Forms.Button();
+            this.eightbutton = new System.Windows.Forms.Button();
+            this.sevenbutton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // zerobutton
+            // 
+            this.zerobutton.Location = new System.Drawing.Point(12, 234);
+            this.zerobutton.Name = "zerobutton";
+            this.zerobutton.Size = new System.Drawing.Size(58, 34);
+            this.zerobutton.TabIndex = 0;
+            this.zerobutton.Text = "0";
+            this.zerobutton.UseVisualStyleBackColor = true;
+            this.zerobutton.Click += new System.EventHandler(this.zerobutton_Click);
+            // 
+            // dotbutton
+            // 
+            this.dotbutton.Location = new System.Drawing.Point(76, 234);
+            this.dotbutton.Name = "dotbutton";
+            this.dotbutton.Size = new System.Drawing.Size(58, 34);
+            this.dotbutton.TabIndex = 1;
+            this.dotbutton.Text = ".";
+            this.dotbutton.UseVisualStyleBackColor = true;
+            this.dotbutton.Click += new System.EventHandler(this.dotbutton_Click);
+            // 
+            // deletebutton
+            // 
+            this.deletebutton.Location = new System.Drawing.Point(140, 234);
+            this.deletebutton.Name = "deletebutton";
+            this.deletebutton.Size = new System.Drawing.Size(58, 34);
+            this.deletebutton.TabIndex = 2;
+            this.deletebutton.Text = "C";
+            this.deletebutton.UseVisualStyleBackColor = true;
+            this.deletebutton.Click += new System.EventHandler(this.deletebutton_Click);
+            // 
+            // equalbutton
+            // 
+            this.equalbutton.Location = new System.Drawing.Point(204, 114);
+            this.equalbutton.Name = "equalbutton";
+            this.equalbutton.Size = new System.Drawing.Size(58, 154);
+            this.equalbutton.TabIndex = 3;
+            this.equalbutton.Text = "=";
+            this.equalbutton.UseVisualStyleBackColor = true;
+            this.equalbutton.Click += new System.EventHandler(this.equalbutton_Click);
+            // 
+            // threebutton
+            // 
+            this.threebutton.Location = new System.Drawing.Point(140, 194);
+            this.threebutton.Name = "threebutton";
+            this.threebutton.Size = new System.Drawing.Size(58, 34);
+            this.threebutton.TabIndex = 6;
+            this.threebutton.Text = "3";
+            this.threebutton.UseVisualStyleBackColor = true;
+            this.threebutton.Click += new System.EventHandler(this.threebutton_Click);
+            // 
+            // twobutton
+            // 
+            this.twobutton.Location = new System.Drawing.Point(76, 194);
+            this.twobutton.Name = "twobutton";
+            this.twobutton.Size = new System.Drawing.Size(58, 34);
+            this.twobutton.TabIndex = 5;
+            this.twobutton.Text = "2";
+            this.twobutton.UseVisualStyleBackColor = true;
+            this.twobutton.Click += new System.EventHandler(this.twobutton_Click);
+            // 
+            // onebutton
+            // 
+            this.onebutton.Location = new System.Drawing.Point(12, 194);
+            this.onebutton.Name = "onebutton";
+            this.onebutton.Size = new System.Drawing.Size(58, 34);
+            this.onebutton.TabIndex = 4;
+            this.onebutton.Text = "1";
+            this.onebutton.UseVisualStyleBackColor = true;
+            this.onebutton.Click += new System.EventHandler(this.onebutton_Click);
+            // 
+            // sixbutton
+            // 
+            this.sixbutton.Location = new System.Drawing.Point(140, 154);
+            this.sixbutton.Name = "sixbutton";
+            this.sixbutton.Size = new System.Drawing.Size(58, 34);
+            this.sixbutton.TabIndex = 9;
+            this.sixbutton.Text = "6";
+            this.sixbutton.UseVisualStyleBackColor = true;
+            this.sixbutton.Click += new System.EventHandler(this.sixbutton_Click);
+            // 
+            // fivebutton
+            // 
+            this.fivebutton.Location = new System.Drawing.Point(76, 154);
+            this.fivebutton.Name = "fivebutton";
+            this.fivebutton.Size = new System.Drawing.Size(58, 34);
+            this.fivebutton.TabIndex = 8;
+            this.fivebutton.Text = "5";
+            this.fivebutton.UseVisualStyleBackColor = true;
+            this.fivebutton.Click += new System.EventHandler(this.fivebutton_Click);
+            // 
+            // fourbutton
+            // 
+            this.fourbutton.Location = new System.Drawing.Point(12, 154);
+            this.fourbutton.Name = "fourbutton";
+            this.fourbutton.Size = new System.Drawing.Size(58, 34);
+            this.fourbutton.TabIndex = 7;
+            this.fourbutton.Text = "4";
+            this.fourbutton.UseVisualStyleBackColor = true;
+            this.fourbutton.Click += new System.EventHandler(this.fourbutton_Click);
+            // 
+            // mulbutton
+            // 
+            this.mulbutton.Location = new System.Drawing.Point(140, 74);
+            this.mulbutton.Name = "mulbutton";
+            this.mulbutton.Size = new System.Drawing.Size(58, 34);
+            this.mulbutton.TabIndex = 12;
+            this.mulbutton.Text = "X";
+            this.mulbutton.UseVisualStyleBackColor = true;
+            this.mulbutton.Click += new System.EventHandler(this.mulbutton_Click);
+            // 
+            // minusbutton
+            // 
+            this.minusbutton.Location = new System.Drawing.Point(76, 74);
+            this.minusbutton.Name = "minusbutton";
+            this.minusbutton.Size = new System.Drawing.Size(58, 34);
+            this.minusbutton.TabIndex = 11;
+            this.minusbutton.Text = "-";
+            this.minusbutton.UseVisualStyleBackColor = true;
+            this.minusbutton.Click += new System.EventHandler(this.minusbutton_Click);
+            // 
+            // plusbutton
+            // 
+            this.plusbutton.Location = new System.Drawing.Point(12, 74);
+            this.plusbutton.Name = "plusbutton";
+            this.plusbutton.Size = new System.Drawing.Size(58, 34);
+            this.plusbutton.TabIndex = 10;
+            this.plusbutton.Text = "+";
+            this.plusbutton.UseVisualStyleBackColor = true;
+            this.plusbutton.Click += new System.EventHandler(this.plusbutton_Click);
+            // 
+            // dividebutton
+            // 
+            this.dividebutton.Location = new System.Drawing.Point(204, 74);
+            this.dividebutton.Name = "dividebutton";
+            this.dividebutton.Size = new System.Drawing.Size(58, 34);
+            this.dividebutton.TabIndex = 13;
+            this.dividebutton.Text = "/";
+            this.dividebutton.UseVisualStyleBackColor = true;
+            this.dividebutton.Click += new System.EventHandler(this.dividebutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 29);
+            this.label1.TabIndex = 14;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // ninebutton
+            // 
+            this.ninebutton.Location = new System.Drawing.Point(140, 114);
+            this.ninebutton.Name = "ninebutton";
+            this.ninebutton.Size = new System.Drawing.Size(58, 34);
+            this.ninebutton.TabIndex = 17;
+            this.ninebutton.Text = "9";
+            this.ninebutton.UseVisualStyleBackColor = true;
+            this.ninebutton.Click += new System.EventHandler(this.ninebutton_Click);
+            // 
+            // eightbutton
+            // 
+            this.eightbutton.Location = new System.Drawing.Point(76, 114);
+            this.eightbutton.Name = "eightbutton";
+            this.eightbutton.Size = new System.Drawing.Size(58, 34);
+            this.eightbutton.TabIndex = 16;
+            this.eightbutton.Text = "8";
+            this.eightbutton.UseVisualStyleBackColor = true;
+            this.eightbutton.Click += new System.EventHandler(this.eightbutton_Click);
+            // 
+            // sevenbutton
+            // 
+            this.sevenbutton.Location = new System.Drawing.Point(12, 114);
+            this.sevenbutton.Name = "sevenbutton";
+            this.sevenbutton.Size = new System.Drawing.Size(58, 34);
+            this.sevenbutton.TabIndex = 15;
+            this.sevenbutton.Text = "7";
+            this.sevenbutton.UseVisualStyleBackColor = true;
+            this.sevenbutton.Click += new System.EventHandler(this.sevenbutton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 280);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ninebutton);
+            this.Controls.Add(this.eightbutton);
+            this.Controls.Add(this.sevenbutton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dividebutton);
+            this.Controls.Add(this.mulbutton);
+            this.Controls.Add(this.minusbutton);
+            this.Controls.Add(this.plusbutton);
+            this.Controls.Add(this.sixbutton);
+            this.Controls.Add(this.fivebutton);
+            this.Controls.Add(this.fourbutton);
+            this.Controls.Add(this.threebutton);
+            this.Controls.Add(this.twobutton);
+            this.Controls.Add(this.onebutton);
+            this.Controls.Add(this.equalbutton);
+            this.Controls.Add(this.deletebutton);
+            this.Controls.Add(this.dotbutton);
+            this.Controls.Add(this.zerobutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button zerobutton;
+        private System.Windows.Forms.Button dotbutton;
+        private System.Windows.Forms.Button deletebutton;
+        private System.Windows.Forms.Button equalbutton;
+        private System.Windows.Forms.Button threebutton;
+        private System.Windows.Forms.Button twobutton;
+        private System.Windows.Forms.Button onebutton;
+        private System.Windows.Forms.Button sixbutton;
+        private System.Windows.Forms.Button fivebutton;
+        private System.Windows.Forms.Button fourbutton;
+        private System.Windows.Forms.Button mulbutton;
+        private System.Windows.Forms.Button minusbutton;
+        private System.Windows.Forms.Button plusbutton;
+        private System.Windows.Forms.Button dividebutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ninebutton;
+        private System.Windows.Forms.Button eightbutton;
+        private System.Windows.Forms.Button sevenbutton;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        string input = "";
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zerobutton_Click(object sender, EventArgs e)
+        {
+           bool isEmpty = string.IsNullOrEmpty(input);
+            if (isEmpty == true)
+            {
+                MessageBox.Show("Cant start with zero");
+            }
+            else
+            {
+                label1.Text += "0";
+                input += "0";
+            }
+        }
+
+        private void onebutton_Click(object sender, EventArgs e)
+        {
+            label1.Text += "1";
+            input += "1";
+        }
+
+        private void twobutton_Click(object sender, EventArgs e)
+        {
+            label1.Text += "2";
+            input += "2";
+        }
+
+        private void threebutton_Click(object sender, EventArgs e)
+        {
+            label1.Text += "3";
+            input += "3";
+        }
+
+        private void fourbutton_Click(object sender, EventArgs e)
+        {
+            label1.Text += "4";
+            input += "4";
+        }
+
+        private void fivebutton_Click(object sender, EventArgs e)
+        {
+            label1.Text += "5";
+            input += "5";
+        }
+
+        private void sixbutton_Click(object sender, EventArgs e)
+        {
+            label1.Text += "6";
+            input += "6";
+        }
+
+        private void sevenbutton_Click(object sender, EventArgs e)
+        {
+            label1.Text += "7";
+            input += "7";
+        }
+
+        private void eightbutton_Click(object sender, EventArgs e)
+        {
+            label1.Text += "8";
+            input += "8";
+        }
+
+        private void ninebutton_Click(object sender, EventArgs e)
+        {
+            label1.Text += "9";
+            input += "9";
+        }
+
+        private void deletebutton_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            input = "";
+            label2.Text = "";
+        }
+
+        private void plusbutton_Click(object sender, EventArgs e)
+        {
+            bool isEmpty = string.IsNullOrEmpty(input);
+            if (isEmpty == true)
+            {
+                MessageBox.Show("Cant start with + ");
+            }
+            
+            else
+            {
+                if (input.EndsWith("+") || input.EndsWith("-") || input.EndsWith("*") || input.EndsWith("/") || input.EndsWith("."))
+                {
+                    MessageBox.Show("Cant accept two experssion togather");
+                }
+                else
+                {
+                    label1.Text += " + ";
+                    input += "+";
+                }
+               
+            }
+        }
+
+        private void minusbutton_Click(object sender, EventArgs e)
+        {
+            bool isEmpty = string.IsNullOrEmpty(input);
+            if (isEmpty == true)
+            {
+                MessageBox.Show("Cant start with - ");
+            }
+            else
+            {
+                if (input.EndsWith("+") || input.EndsWith("-") || input.EndsWith("*") || input.EndsWith("/") || input.EndsWith("."))
+                {
+                    MessageBox.Show("Cant accept two experssion togather");
+                }
+                else
+                {
+                    label1.Text += " - ";
+                    input += "-";
+                }
+
+            }
+        }
+
+        private void mulbutton_Click(object sender, EventArgs e)
+        {
+            bool isEmpty = string.IsNullOrEmpty(input);
+            if (isEmpty == true)
+            {
+                MessageBox.Show("Cant start with X ");
+            }
+            else
+            {
+                if (input.EndsWith("+") || input.EndsWith("-") || input.EndsWith("*") || input.EndsWith("/") || input.EndsWith("."))
+                {
+                    MessageBox.Show("Cant accept two experssion togather");
+                }
+                else
+                {
+                    label1.Text += " X ";
+                    input += "*";
+                }
+
+            }
+        }
+
+        private void dividebutton_Click(object sender, EventArgs e)
+        {
+            bool isEmpty = string.IsNullOrEmpty(input);
+            if (isEmpty == true)
+            {
+                MessageBox.Show("Cant start with / ");
+            }
+            else
+            {
+                if (input.EndsWith("+") || input.EndsWith("-") || input.EndsWith("*") || input.EndsWith("/") || input.EndsWith("."))
+                {
+                    MessageBox.Show("Cant accept two experssion togather");
+                }
+                else
+                {
+                    label1.Text += " / ";
+                    input += "/";
+                }
+
+            }
+        }
+
+        private void equalbutton_Click(object sender, EventArgs e)
+        {
+            bool isEmpty = string.IsNullOrEmpty(input);
+            if (isEmpty == true)
+            {
+                MessageBox.Show("Cant start with = ");
+            }
+            if (input.EndsWith("+") || input.EndsWith("-") || input.EndsWith("*") || input.EndsWith("/") || input.EndsWith("."))
+            {
+                MessageBox.Show("Cant accept two experssion togather");
+            }
+            else
+            {
+                label1.Text += " = ";
+                label2.Text = label1.Text;
+                label1.Text = "";
+                
+            }
+        }
+
+        private void dotbutton_Click(object sender, EventArgs e)
+        {
+            bool isEmpty = string.IsNullOrEmpty(input);
+            if (isEmpty == true)
+            {
+                MessageBox.Show("Cant start with . ");
+            }
+            else
+            {
+                if (input.EndsWith("+") || input.EndsWith("-") || input.EndsWith("*") || input.EndsWith("/") || input.EndsWith("."))
+                {
+                    MessageBox.Show("Cant accept two experssion togather");
+                }
+                else
+                {
+                    label1.Text += ".";
+                    input += ".";
+                }
+
+            }
+        }
+    }
+}
